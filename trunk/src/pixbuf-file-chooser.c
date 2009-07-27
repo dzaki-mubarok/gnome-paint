fcfff0e01e5096ab7eb9ade2542e45060104d636
rogerioferro <rogerioferro@06becd44-3357-11de-a996-7ff70a941223>
2009-07-27 22:45:24 +0000

    - improve file chooser

--- trunk/src/pixbuf-file-chooser.c
+++ trunk/src/pixbuf-file-chooser.c
@@ -32,15 +32,21 @@ static char *last_dir[] = { NULL, NULL, NULL, NULL };
 	(G_TYPE_INSTANCE_GET_PRIVATE ((object), PIXBUF_TYPE_FILE_CHOOSER, PixbufFileChooserPrivate))
 
 
-static GSList* 			pixbuf_get_savable_formats		(void);
 static GdkPixbufFormat*	pixbuf_get_format_by_suffix		(const gchar *suffix);
 static gchar*			get_suffix_from_basename 		(const gchar *basename);
 
 
+enum 
+{
+	TEXT_COL,
+	FILTER_COL,
+	N_COLUMNS
+};
 
 struct _PixbufFileChooserPrivate
 {
-	gint private;
+	GtkWidget	*combo_filter;
+	GSList		*filters;
 };
 
 
@@ -49,6 +55,19 @@ G_DEFINE_TYPE(PixbufFileChooser, pixbuf_file_chooser, GTK_TYPE_FILE_CHOOSER_DIAL
 static void
 pixbuf_file_chooser_finalize (GObject *object)
 {
+	PixbufFileChooserPrivate *priv;
+	GSList *it;
+
+	priv	=	PIXBUF_FILE_CHOOSER (object)->priv;
+
+	for (it = priv->filters; it != NULL; it = it->next)
+    {
+		GtkFileFilter *filter;
+		filter = GTK_FILE_FILTER (it->data);
+		g_object_unref (filter);
+    }
+	g_slist_free (priv->filters);
+	
 	(* G_OBJECT_CLASS (pixbuf_file_chooser_parent_class)->finalize) (object);
 }
 
@@ -66,6 +85,8 @@ static void
 pixbuf_file_chooser_init (PixbufFileChooser *chooser)
 {
 	chooser->priv = PIXBUF_FILE_CHOOSER_GET_PRIVATE (chooser);
+	chooser->priv->combo_filter	= NULL;
+	chooser->priv->filters 		= NULL;
 }
 
 static void
@@ -89,9 +110,9 @@ response_cb (GtkDialog *dlg, gint id, gpointer data)
 
 static void
 add_custom_button_to_dialog (GtkDialog   *dialog,
-			     const gchar *mnemonic_label,
-			     const gchar *stock_id,
-			     gint         response_id)
+			     			 const gchar *mnemonic_label,
+						     const gchar *stock_id,
+						     gint         response_id)
 {
   GtkWidget *button;
 
@@ -104,40 +125,60 @@ add_custom_button_to_dialog (GtkDialog   *dialog,
   gtk_dialog_add_action_widget (GTK_DIALOG (dialog), button, response_id);
 }
 
+
+static gboolean
+pixbuf_file_chooser_add_ext (GtkFileChooser * chooser)
+{
+	gboolean		changed = FALSE;
+	GFile 			*file;
+
+	file 		= gtk_file_chooser_get_file (chooser);
+	if ( file != NULL )
+	{
+		gchar 			*file_name, *suffix;
+		GdkPixbufFormat	*format, *my_format;
+		file_name	= g_file_get_basename (file);
+		g_object_unref (file);
+
+		suffix		= get_suffix_from_basename (file_name);
+		format		= pixbuf_get_format_by_suffix (suffix);
+
+		my_format 	= pixbuf_file_chooser_get_format (PIXBUF_FILE_CHOOSER (chooser));
+	
+		if (format != my_format)
+		{
+			gchar *new_file_name, *extension;
+			if (format != NULL)
+			{
+				*--suffix = '\0';
+			}
+			extension		= gdk_pixbuf_format_get_name ( my_format );
+			new_file_name	= g_strdup_printf ("%s.%s", file_name, extension);
+			gtk_file_chooser_set_current_name ( chooser, new_file_name);
+			g_free (new_file_name);
+			g_free (extension);
+			changed = TRUE;
+		}
+		g_free (file_name);
+	}
+
+	return changed;
+}
+
 static void
 save_response_cb (GtkDialog *dlg, gint id, gpointer data)
 {
 	gboolean _ok = TRUE;
-	GFile *file;
-	gchar *file_name, *suffix;
-	GdkPixbufFormat *format, *my_format;
 
 	if (id != GTK_RESPONSE_OK)
 		return;
 
-	file 		= gtk_file_chooser_get_file (GTK_FILE_CHOOSER (dlg));
-	file_name	= g_file_get_basename (file);
-	suffix		= get_suffix_from_basename (file_name);
-	format		= pixbuf_get_format_by_suffix (suffix);
-	g_object_unref (file);
-
-	my_format 	= pixbuf_file_chooser_get_format (PIXBUF_FILE_CHOOSER (dlg));
-	
-	if (format != my_format)
+	if (pixbuf_file_chooser_add_ext (GTK_FILE_CHOOSER (dlg)))
 	{
-		gchar *new_file_name, *extension;
-		if (format != NULL)
-		{
-			*--suffix = '\0';
-		}
-		extension		= gdk_pixbuf_format_get_name ( my_format );
-		new_file_name	= g_strdup_printf ("%s.%s", file_name, extension);
-		gtk_file_chooser_set_current_name ( GTK_FILE_CHOOSER (dlg), new_file_name);
-		g_free (new_file_name);
-		g_free (extension);
 		/* We changed the file name, then
 		   we need to test if the file already exist*/
-		file 		= gtk_file_chooser_get_file (GTK_FILE_CHOOSER (dlg));
+		GFile	*file;
+		file	= gtk_file_chooser_get_file (GTK_FILE_CHOOSER (dlg));
 		if ( g_file_query_exists (file, NULL) )
 		{
 			_ok = FALSE;
@@ -155,7 +196,7 @@ save_response_cb (GtkDialog *dlg, gint id, gpointer data)
 		}
 		g_object_unref (file);
 	}
-	g_free (file_name);
+
 
 	if (_ok)
 	{
@@ -163,104 +204,185 @@ save_response_cb (GtkDialog *dlg, gint id, gpointer data)
 	}
 }
 
+static GSList *
+pixbuf_get_filters ( gboolean savable )
+{
+	GtkFileFilter	*all_img_filter;
+	GSList *list;
+	GSList *write_list = NULL;
+	GSList *it;
+
+	if (!savable)
+	{
+		/* All Image Filter */
+		all_img_filter = gtk_file_filter_new ();
+		gtk_file_filter_set_name (all_img_filter, _("All Images"));
+		write_list = g_slist_prepend (write_list, all_img_filter);
+	}
+
+	list = gdk_pixbuf_get_formats ();
+
+	for (it = list; it != NULL; it = it->next)
+	{
+		GdkPixbufFormat *format;
+
+		format = (GdkPixbufFormat*) it->data;
+		if (gdk_pixbuf_format_is_writable (format) || (!savable))
+		{
+			gint i;
+			gchar **mime_types, **pattern;
+			gchar  *description;
+			GtkFileFilter	*filter;
+			filter	= gtk_file_filter_new ();
+
+			/* Filter name.*/
+			description	= gdk_pixbuf_format_get_description (format);
+			gtk_file_filter_set_name (filter, description);
+			g_free (description);
+
+			mime_types = gdk_pixbuf_format_get_mime_types (format);
+			for (i = 0; mime_types[i] != NULL; i++) 
+			{
+				gtk_file_filter_add_mime_type (filter, mime_types[i]);
+				if (!savable)
+				{
+					gtk_file_filter_add_mime_type (all_img_filter, mime_types[i]);
+				}
+			}
+			g_strfreev (mime_types);
+
+			pattern = gdk_pixbuf_format_get_extensions (format);
+			for (i = 0; pattern[i] != NULL; i++) 
+			{
+				gchar *tmp;
+				tmp = g_strconcat ("*.", pattern[i], NULL);
+				gtk_file_filter_add_pattern (filter, tmp);
+				if (!savable)
+				{
+					gtk_file_filter_add_pattern (all_img_filter, tmp);
+				}
+				g_free (tmp);
+			}
+			g_strfreev (pattern);
+	
+
+			/* attach GdkPixbufFormat to filter, see also
+			 * pixbuf_file_chooser_get_format. */
+			g_object_set_data (G_OBJECT (filter), FILE_FORMAT_KEY, format );
+			
+			write_list = g_slist_prepend (write_list, filter);
+		}
+	}
+
+	g_slist_free (list);
+	write_list = g_slist_reverse (write_list);
+
+	return write_list;
+}
+
+void
+chage_type_cb (GtkComboBox *combo, GtkFileChooser * chooser)
+{
+	GtkTreeModel 	*model;
+	GtkTreeIter 	iter;
+	GtkFileChooserAction	action;
+
+	model = gtk_combo_box_get_model (combo);
+
+	if (gtk_combo_box_get_active_iter (combo, &iter))
+	{
+      	GtkFileFilter	*filter;
+		
+      	gtk_tree_model_get (model, &iter,
+	                        FILTER_COL, &filter,
+	                        -1);
+
+		gtk_file_chooser_set_filter (chooser, filter);
+	}
+
+	action = gtk_file_chooser_get_action (GTK_FILE_CHOOSER (chooser));
+	if (action == GTK_FILE_CHOOSER_ACTION_SAVE)
+	{
+		if ( pixbuf_file_chooser_add_ext (chooser) )
+		{
+			gchar 			*dir;
+			dir = gtk_file_chooser_get_current_folder (chooser);
+			gtk_file_chooser_set_current_folder ( chooser, dir  );
+			g_free (dir);
+		}
+	}
+}
+
 static void
 pixbuf_file_chooser_add_filter (PixbufFileChooser *chooser)
 {
-	GSList			*it;
-	GSList			*formats;
- 	GtkFileFilter	*all_file_filter;
-	GtkFileFilter	*all_img_filter;
-	GtkFileChooserAction action;
+	GSList					*it;
+	GSList					*filters;
+	GtkWidget 				*combo;
+	GtkWidget 				*box;
+	GtkListStore 			*store;
+	GtkCellRenderer 		*renderer;
+	GtkFileChooserAction	action;
 
 	action = gtk_file_chooser_get_action (GTK_FILE_CHOOSER (chooser));
 
-	if (action != GTK_FILE_CHOOSER_ACTION_SAVE && action != GTK_FILE_CHOOSER_ACTION_OPEN)
+	if (action == GTK_FILE_CHOOSER_ACTION_OPEN)
 	{
-		return;
-	}
-
-	if (action == GTK_FILE_CHOOSER_ACTION_SAVE)
-	{
-		formats = pixbuf_get_savable_formats ();
-	}
-	else
-	{
+		GtkFileFilter			*all_file_filter;
 		/* All Files Filter */
 		all_file_filter = gtk_file_filter_new ();
 		gtk_file_filter_set_name (all_file_filter, _("All Files"));
 		gtk_file_filter_add_pattern (all_file_filter, "*");
 
-		/* All Image Filter */
-		all_img_filter = gtk_file_filter_new ();
-		gtk_file_filter_set_name (all_img_filter, _("All Images"));
-
-		/* Add filter to filechooser */
-		gtk_file_chooser_add_filter (GTK_FILE_CHOOSER (chooser), all_file_filter);
-		gtk_file_chooser_add_filter (GTK_FILE_CHOOSER (chooser), all_img_filter);
-		gtk_file_chooser_set_filter (GTK_FILE_CHOOSER (chooser), all_img_filter);		
-
-		formats = gdk_pixbuf_get_formats ();
+		filters = pixbuf_get_filters (FALSE);
+		filters = g_slist_prepend (filters, all_file_filter);
+	}
+	else
+	{	
+		filters = pixbuf_get_filters (TRUE);
 	}
 
-	/* Image filters */
-	for (it = formats; it != NULL; it = it->next) 
+	store = gtk_list_store_new (N_COLUMNS, G_TYPE_STRING, G_TYPE_POINTER);
+
+	/* filters */
+	for (it = filters; it != NULL; it = it->next) 
 	{
-		gint i;
-		gchar **mime_types, **pattern, *tmp;
-		gchar  *description, *extension;
+		GtkTreeIter 	iter;
 		GtkFileFilter	*filter;
-		GdkPixbufFormat *format;
 
-		filter			= gtk_file_filter_new ();
-		format 			= (GdkPixbufFormat*) it->data;
-
-		/* Filter name.*/
-		description	= gdk_pixbuf_format_get_description (format);
-		gtk_file_filter_set_name (filter, description);
-		g_free (description);
-
-		/* Add filter to filechooser */
-		gtk_file_chooser_add_filter (GTK_FILE_CHOOSER (chooser), filter);
-		extension	= gdk_pixbuf_format_get_name (format);
-		if (action == GTK_FILE_CHOOSER_ACTION_SAVE)
-		if (g_strcmp0 ("bmp" , extension) == 0)
-		{
-			gtk_file_chooser_set_filter (GTK_FILE_CHOOSER (chooser), filter);
-		}
-		g_free (extension);
-
-		mime_types = gdk_pixbuf_format_get_mime_types ((GdkPixbufFormat *) it->data);
-		for (i = 0; mime_types[i] != NULL; i++) 
-		{
-			gtk_file_filter_add_mime_type (filter, mime_types[i]);
-			if (action == GTK_FILE_CHOOSER_ACTION_OPEN)
-			{
-				gtk_file_filter_add_mime_type (all_img_filter, mime_types[i]);
-			}
-		}
-		g_strfreev (mime_types);
-
-		pattern = gdk_pixbuf_format_get_extensions ((GdkPixbufFormat *) it->data);
-		for (i = 0; pattern[i] != NULL; i++) 
-		{
-			tmp = g_strconcat ("*.", pattern[i], NULL);
-			gtk_file_filter_add_pattern (filter, tmp);
-			if (action == GTK_FILE_CHOOSER_ACTION_OPEN)
-			{
-				gtk_file_filter_add_pattern (all_img_filter, tmp);
-			}
-			g_free (tmp);
-		}
-		g_strfreev (pattern);
-	
-
-		/* attach GdkPixbufFormat to filter, see also
-		 * pixbuf_file_chooser_get_format. */
-		g_object_set_data (G_OBJECT (filter), FILE_FORMAT_KEY, format );
+		filter =  (GtkFileFilter*)g_object_ref_sink (it->data);
+		
+		/* Add filter to combo */
+		gtk_list_store_append (store, &iter);
+		gtk_list_store_set (store, &iter,
+					  		TEXT_COL, gtk_file_filter_get_name (filter),
+			                FILTER_COL, filter,
+					  		-1);
 	}
-	g_slist_free (formats);
+	/*store filter list to unref*/
+	chooser->priv->filters = filters;
 
+	box		= gtk_hbox_new (FALSE,0);
+	combo	= gtk_combo_box_new_with_model (GTK_TREE_MODEL (store));
+
+    renderer = gtk_cell_renderer_text_new ();
+    gtk_cell_layout_pack_start (GTK_CELL_LAYOUT (combo), renderer, TRUE);
+    gtk_cell_layout_set_attributes (GTK_CELL_LAYOUT (combo), renderer,
+                                    "text", TEXT_COL,
+								    NULL);	
+
+	gtk_box_pack_end (GTK_BOX(box), combo, FALSE, TRUE, 0);
+	g_signal_connect 	(combo, "changed",
+			  			G_CALLBACK (chage_type_cb),
+			  			chooser);
+	gtk_widget_show (combo);
+	gtk_combo_box_set_focus_on_click (GTK_COMBO_BOX (combo), FALSE);
+	gtk_file_chooser_set_extra_widget (GTK_FILE_CHOOSER (chooser), box);
+
+	chooser->priv->combo_filter = combo;
+	gtk_combo_box_set_active (GTK_COMBO_BOX (combo), 1);
 }
+
 
 GtkWidget *
 pixbuf_file_chooser_new (GtkFileChooserAction action)
@@ -304,10 +426,6 @@ pixbuf_file_chooser_new (GtkFileChooserAction action)
 			g_assert_not_reached ();
 	}
 
-	if (action != GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER) 
-	{
-		pixbuf_file_chooser_add_filter (PIXBUF_FILE_CHOOSER (chooser));
-	}
 
 	if (last_dir[action] == NULL) 
 	{
@@ -330,6 +448,11 @@ pixbuf_file_chooser_new (GtkFileChooserAction action)
 
 	gtk_file_chooser_set_do_overwrite_confirmation (GTK_FILE_CHOOSER (chooser), TRUE);
 
+	if (action != GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER)
+	{
+		pixbuf_file_chooser_add_filter (PIXBUF_FILE_CHOOSER (chooser));
+	}
+	
 	return chooser;
 }
 
@@ -353,31 +476,44 @@ pixbuf_file_chooser_get_format (PixbufFileChooser *chooser)
 void 
 pixbuf_file_chooser_set_current_filter (PixbufFileChooser *chooser, const gchar *extension)
 {
-	GdkPixbufFormat* format;
+	GdkPixbufFormat			*format;
+
 	g_return_if_fail (extension != NULL);
 	g_return_if_fail (PIXBUF_IS_FILE_CHOOSER (chooser));
-	
+
 	format = pixbuf_get_format_by_suffix (extension);
 
 	if (format != NULL)
 	{
-		GSList *it;
-		GSList *list = NULL;
-		list =  gtk_file_chooser_list_filters ( GTK_FILE_CHOOSER (chooser));
-		for (it = list; it != NULL; it = it->next)
+		GtkWidget *combo = chooser->priv->combo_filter;
+
+		if (combo != NULL)
 		{
-			GtkFileFilter *filter;
-			GdkPixbufFormat* it_format;
-			filter = (GtkFileFilter*)it->data;
-			it_format = g_object_get_data (G_OBJECT (filter), FILE_FORMAT_KEY);
-			if (format == it_format)
-			{
-				gtk_file_chooser_set_filter (GTK_FILE_CHOOSER (chooser), filter);
-				break;
+			GtkTreeModel 	*model;
+			GtkTreeIter 	iter;
+			gboolean		valid;
+			model = gtk_combo_box_get_model (combo);
+			valid = gtk_tree_model_get_iter_first (model, &iter);
+			while (valid)
+		    {
+			  	GtkFileFilter	*filter;
+				GdkPixbufFormat	*it_format;
+			  	gtk_tree_model_get (model, &iter,
+					                FILTER_COL, &filter,
+					                -1);
+				it_format = g_object_get_data (G_OBJECT (filter), FILE_FORMAT_KEY);
+				if (format == it_format)
+				{
+					gtk_combo_box_set_active_iter (GTK_COMBO_BOX (combo), &iter);
+					valid = FALSE;
+				}
+				else
+				{
+					valid = gtk_tree_model_iter_next (model, &iter);
+				}
 			}
 		}
-		g_slist_free (list);
-	}
+    }
 }
 
 gchar *
@@ -404,32 +540,6 @@ pixbuf_file_chooser_get_name (PixbufFileChooser *chooser)
 	}
 
 	return file_name;
-}
-
-static GSList*
-pixbuf_get_savable_formats (void)
-{
-	GSList *list;
-	GSList *write_list = NULL;
-	GSList *it;
-
-	list = gdk_pixbuf_get_formats ();
-
-	for (it = list; it != NULL; it = it->next)
-	{
-		GdkPixbufFormat *format;
-
-		format = (GdkPixbufFormat*) it->data;
-		if (gdk_pixbuf_format_is_writable (format))
-		{
-			write_list = g_slist_prepend (write_list, format);
-		}
-	}
-
-	g_slist_free (list);
-	write_list = g_slist_reverse (write_list);
-
-	return write_list;
 }
 
 static GdkPixbufFormat*
